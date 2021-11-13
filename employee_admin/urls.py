871c1da8c179593add7927a7417f3e5a1b8c270c
muthupandyN <muthupandynagendran@gmail.com>
2021-11-13 23:05:04 +0530

    final deployment commit

---
+++ employee_admin/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from .import views
+
+
+urlpatterns = [
+
+    path('',views.adminLogin,name='login'),
+    path('logout/',views.adminlogout,name='admin_logout'),
+    path('emplyee/',views.adminEmployee,name='admin_employee'),
+    path('employeesave/',views.employeeSave,name='employee_save'),
+    path('employeeList/',views.employeeList,name='employee_list'),
+    path('employeeupdate/',views.employeeUpdate,name='employee_update'),
+    path('employeeUpdatesave/',views.employeeUpdateSave,name='emp_updatesave'),
+    path('empdelete/',views.employeeDelete,name='emp_delte'),
+    path('employeesearch/',views.employeeSearch,name='emp_search')
+]
