1d524bcf605f074dc57049c62ae093a873e38e43
muthupandyN <muthupandynagendran@gmail.com>
2021-11-14 09:46:55 +0530

    static file changed

--- employee/settings.py
+++ employee/settings.py
@@ -135,5 +135,4 @@ STATIC_URL = '/static/'
 
 STATIC_DIRS = [os.path.join(BASE_DIR,'static')]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-django_heroku.settings(locals())
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
